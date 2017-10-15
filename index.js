84c26313f9cfd152fd5098d2db5c6805f80e6c2e
Jerico Alcaras <alcarasj@tcd.ie>
2017-10-15 22:38:22 +0100

    Added executables for portable deployment.

--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-var net = require('net');
+const net = require('net');
 
-var HOST = '127.0.0.1';
-var PORT = 5000;
+const HOST = '127.0.0.1';
+const PORT = 5000;
+const STUDENT_NUMBER = 14317110
 
 // Create a server instance, and chain the listen function to it
 // The function passed to net.createServer() becomes the event handler for the 'connection' event
 // The sock object the callback function receives UNIQUE for each connection
-net.createServer(function(sock) {
+var server = net.createServer()
+server.listen(PORT, HOST);
+console.log('Server listening on ' + HOST +':'+ PORT);
+server.on('connection', (socket) => {
 
     // We have a connection - a socket object is assigned to the connection automatically
-    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+    console.log('CONNECTED: ' + socket.remoteAddress +':'+ socket.remotePort);
 
     // Add a 'data' event handler to this instance of socket
-    sock.on('data', function(data) {
+    socket.on('data', (data) => {
 
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        // Write the data back to the socket, the client will receive it as data from the server
-        sock.write('You said "' + data + '"');
+        console.log('DATA ' + socket.remoteAddress + ': ' + data);
+
+        switch (data.toString()) {
+          case 'HELO text\n':
+            socket.write('HELO text\nIP:${HOST}\nPort:${PORT}\nStudentID:${STUDENT_NUMBER}\n')
+          break;
+          case 'KILL_SERVICE\n':
+            server.close(() => {
+              console.log('Server closed.');
+            });
+            socket.destroy();
+          default:
+          break;
+        }
 
     });
 
     // Add a 'close' event handler to this instance of socket
-    sock.on('close', function(data) {
-        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    socket.on('close', (data) => {
+        console.log('CLOSED: ' + socket.remoteAddress +' '+ socket.remotePort);
     });
-
-}).listen(PORT, HOST);
-
-console.log('Server listening on ' + HOST +':'+ PORT);
+});
