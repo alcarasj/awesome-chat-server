dd1611b024ffc884086321d52bc323a59c849e7e
Jerico Alcaras <alcarasj@tcd.ie>
2017-11-17 11:01:57 +0000

    Docker preparation.

--- start.sh
+++ start.sh
@@ -3,5 +3,13 @@ if [ $# -eq 0 ]
     echo "No arguments supplied."
     echo "USAGE: start.sh PORT_NUMBER"
 fi
+case "$OSTYPE" in
+  solaris*) echo "Solaris is not supported." ;;
+  darwin*)  ./server-macos $1 ;; 
+  linux*)   ./server-linux $1 ;;
+  bsd*)     echo "BSD is not supported." ;;
+  msys*)    start server-win.exe $1 ;;
+  *)        echo "Unknown: $OSTYPE is not supported." ;;
+esac
 start server-win.exe $1
 exit
